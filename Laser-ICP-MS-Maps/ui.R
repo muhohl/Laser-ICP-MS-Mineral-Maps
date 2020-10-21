713e7d2ba77c77f4a2b2f97c6f71da5317c9f9fa
muhohl <max.hohl@utas.edu.au>
2020-10-21 21:55:45 +1100

    First commit. Setting things the project slowely up.

---
+++ Laser-ICP-MS-Maps/ui.R
@@ -0,0 +1,32 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Laser Icp-Ms Maps"),
+
+    sidebarLayout(
+        sidebarPanel(
+            # Uploat data set
+            fileInput("upload",
+                      "Laser Data")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            # if more than 12 elements are selected I should create more plot 
+            # Outputs
+            plotOutput("distPlot")
+        )
+    )
+))
